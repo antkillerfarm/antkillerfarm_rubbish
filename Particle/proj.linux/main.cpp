1d65648f34caa453dcbd4894caf0ddd7736515fe
antkillerfarm <antkillerfarm@gmail.com>
2015-04-12 19:26:40 +0800

    add Particle Demo

---
+++ Particle/proj.linux/main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include "Classes/AppDelegate.h"
+#include "Classes/CCQApplication.h"
+
+int main(int argc, char *argv[])
+{
+	AppDelegate app(argc , argv);
+	return cocos2d::CCQApplication::getInstance()->run();
+}
