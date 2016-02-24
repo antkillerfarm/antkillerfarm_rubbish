8f80209be85f7685adfbfe95c19a6eb7bafe1256
antkillerfarm <antkillerfarm@gmail.com>
2016-02-24 20:39:22 +0800

    t

--- pipe/writepipe.c
+++ pipe/writepipe.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void main() { 
-  FILE * in_file; 
-  int count = 1; 
-  char buf[80]; 
-  in_file = fopen("mypipe", "r"); 
-  if (in_file == NULL) { 
-    printf("Error in fdopen./n"); 
-  } 
-  while ((count = fread(buf, 1, 80, in_file)) > 0) 
-    printf("received from pipe: %s/n", buf); 
-  fclose(in_file); 
+int main(int argc, char *argv[])
+{
+  FILE * out_file;
+  int count = 1;
+  char buf[80];
+  out_file = fopen("mypipe", "w");
+  if (out_file == NULL) {
+    printf("Error opening pipe.");
+    return 0;
+  }
+  sprintf(buf,"this is test data for the named pipe example\n");
+  fwrite(buf, 1, 80, out_file);
+  fclose(out_file);
 }
